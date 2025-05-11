f7b0835831b64a9e6b597f8a07cafa912a8b7e82
SandithP2001 <sandithpivithuru@gmail.com>
2025-05-11 23:50:45 +0530

    Create User Management APIs

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const path = require("path");
 const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
@@ -26,7 +27,7 @@ app.use(express.json());
 
 //Routes
 app.use("/api/auth", authRoutes);
-// app.use("/api/user", userRoutes);
+app.use("/api/users", userRoutes);
 // app.use("/api/task", taskRoutes);
 // app.use("api/reports", reportRoutes);
 
